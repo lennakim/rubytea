3a96d8cc9e2418e7f771c19c9c7a946ec9ed908a
shooter <shooter.hero@gmail.com>
2014-12-30 16:44:56 +0800

    mina deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,47 +1,29 @@
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-# require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
-# require 'mina/rvm'    # for rvm support. (http://rvm.io)
+require 'mina/rbenv'
 
-# Basic settings:
-#   domain       - The hostname to SSH to.
-#   deploy_to    - Path to deploy into.
-#   repository   - Git repo to clone from. (needed by mina/git)
-#   branch       - Branch name to deploy. (needed by mina/git)
+set :domain, 'rubytea.com'
+set :user, 'deploy'
 
-set :domain, 'foobar.com'
-set :deploy_to, '/var/www/foobar.com'
-set :repository, 'git://...'
+set :deploy_to, '/home/deploy/www/rubytea'
+set :current_path, 'current'
+set :app_path,  "#{deploy_to}/#{current_path}"
+
+set :repository, 'git@github.com:sundevilyang/rubytea.git'
 set :branch, 'master'
+set :keep_releases, 20
+set :forward_agent, true  # SSH forward_agent.
 
-# For system-wide RVM install.
-#   set :rvm_path, '/usr/local/rvm/bin/rvm'
+set :unicorn_pid, lambda { "#{deploy_to}/#{shared_path}/tmp/pids/unicorn.pid" }
 
-# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
-# They will be linked in the 'deploy:link_shared_paths' step.
 set :shared_paths, ['config/database.yml', 'log']
 
-# Optional settings:
-#   set :user, 'foobar'    # Username in the server to SSH to.
-#   set :port, '30000'     # SSH port number.
-#   set :forward_agent, true     # SSH forward_agent.
-
-# This task is the environment that is loaded for most commands, such as
-# `mina deploy` or `mina rake`.
 task :environment do
-  # If you're using rbenv, use this to load the rbenv environment.
-  # Be sure to commit your .rbenv-version to your repository.
-  # invoke :'rbenv:load'
-
-  # For those using RVM, use this to load an RVM version@gemset.
-  # invoke :'rvm:use[ruby-1.9.3-p125@default]'
+  invoke :'rbenv:load'
 end
 
-# Put any custom mkdir's in here for when `mina setup` is ran.
-# For Rails apps, we'll make some of the shared paths that are shared between
-# all releases.
-task :setup => :environment do
+task setup: :environment do
   queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
 
@@ -53,10 +35,8 @@ task :setup => :environment do
 end
 
 desc "Deploys the current version to the server."
-task :deploy => :environment do
+task deploy: :environment do
   deploy do
-    # Put things that will set up an empty directory into a fully set-up
-    # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
@@ -65,16 +45,44 @@ task :deploy => :environment do
     invoke :'deploy:cleanup'
 
     to :launch do
-      queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
+      invoke :'unicorn:restart'
       queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
     end
   end
 end
 
-# For help in making your deploy script, see the Mina documentation:
-#
-#  - http://nadarei.co/mina
-#  - http://nadarei.co/mina/tasks
-#  - http://nadarei.co/mina/settings
-#  - http://nadarei.co/mina/helpers
+namespace :unicorn do
+  desc "Start Unicorn"
+  task start: :environment do
+    queue 'echo "-----> Start Unicorn"'
+    queue! %{
+      cd #{app_path}
+      bundle exec unicorn_rails -E production -c config/unicorn.rb -D
+    }
+  end
 
+  desc "Stop Unicorn"
+  task stop: :environment do
+    queue 'echo "-----> Stop Unicorn"'
+    queue! %{
+      test -s "#{unicorn_pid}" && kill -QUIT `cat "#{unicorn_pid}"` && echo "Stop Ok" && exit 0
+      echo >&2 "Not running"
+    }
+  end
+
+  desc "Restart Unicorn"
+  task restart: :environment do
+    invoke :'unicorn:stop'
+    invoke :'unicorn:start'
+  end
+end
+
+namespace :assets do
+  desc "assets clear"
+  task clear: :environment do
+    queue  %{
+      cd #{app_path}
+      bundle exec rake assets:clobber RAILS_ENV=production
+    }
+  end
+end
