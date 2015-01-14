1e362f62c9bb87a7f9caa76f35ae7d2de44a84e9
shooter <shooter.hero@gmail.com>
2015-01-14 21:47:04 +0800

    ad event model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150107140933) do
+ActiveRecord::Schema.define(version: 20150114132336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string   "title"
+    t.string   "subtitle"
+    t.text     "body"
+    t.integer  "limit_num"
+    t.string   "price"
+    t.string   "address"
+    t.datetime "start_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
