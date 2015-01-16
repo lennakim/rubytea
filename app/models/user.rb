e2a4e3d82cc4b555962a15add8dfabfb70d533a3
shooter <shooter.hero@gmail.com>
2015-01-16 17:07:56 +0800

    add events btn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  scope :recent, -> { order(created_at: :desc) }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
