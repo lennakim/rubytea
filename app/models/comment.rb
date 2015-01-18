cdcc0be8d69b371ae96a9a268d6805e25d482131
shooter <shooter.hero@gmail.com>
2015-01-18 08:48:24 +0800

    rm comments author column

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
-  belongs_to :commentable, polymorphic: true
   belongs_to :user
+  belongs_to :commentable, polymorphic: true
+
+  scope :recent, -> { order(created_at: :desc) }
 end
---
+++ db/migrate/20150118004632_rm_author_to_comments.rb
@@ -0,0 +1,5 @@
+class RmAuthorToComments < ActiveRecord::Migration
+  def change
+    remove_column :comments, :author
+  end
+end
